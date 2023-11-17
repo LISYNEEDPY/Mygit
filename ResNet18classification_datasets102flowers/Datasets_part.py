09a2265d922e894ea41665517874104dc1edb450
Cyh <93377600+LISYNEEDPY@users.noreply.github.com>
2023-11-17 11:40:25 +0800

    code changed

--- ResNet18classification_datasets102flowers/Datasets_part.py
+++ ResNet18classification_datasets102flowers/Datasets_part.py
@@ -5,10 +5,10 @@ import numpy as np
 import os
 from PIL import Image
 import shutil
-labels = scipy.io.loadmat('PATH/imagelabels.mat')#该地址为imagelabels.mat的相对地址
+labels = scipy.io.loadmat('PATH/imagelabels.mat')#该地址为imagelabels.mat的地址
 labels = np.array(labels['labels'][0]) - 1
 print("labels:", labels)
-setid = scipy.io.loadmat('PATH/setid.mat')#该地址为setid.mat的相对地址
+setid = scipy.io.loadmat('PATH/setid.mat')#该地址为setid.mat的地址
 
 validation = np.array(setid['valid'][0]) - 1
 np.random.shuffle(validation)#valid字段:总共有1020列，每10列为一类花卉的图片，每列上的数字代表图片号。
@@ -20,7 +20,7 @@ train = np.array(setid['tstid'][0]) - 1
 np.random.shuffle(test)#-tstid字段:总共有6149列，每一类花卉的列数不定，每列上的数字代表图片号。
 flower_dir = list()
 
-for img in os.listdir("PATH/jpg"):#该地址为源数据图片的相对地址         
+for img in os.listdir("PATH/jpg"):#该地址为源数据图片的地址         
     flower_dir.append(os.path.join("PATH/jpg", img))
 
 flower_dir.sort()
@@ -46,7 +46,7 @@ for tid in train:
     despath = os.path.join(class_path, base_path)
     print("despath:", despath)
     img.save(despath)
-des_folder_validation = "validation"#该地址为新建的验证数据集文件夹的相对地址
+des_folder_validation = "validation"#该地址为新建的验证数据集文件夹的地址
 
 for tid in validation:
     img = Image.open(flower_dir[tid])
@@ -67,7 +67,7 @@ for tid in validation:
     despath = os.path.join(class_path, base_path)
     print("despath:", despath)
     img.save(despath)
-des_folder_test = "test"#该地址为新建的测试数据集文件夹的绝对地址
+des_folder_test = "test"#该地址为新建的测试数据集文件夹的地址
 
 for tid in test:
     img = Image.open(flower_dir[tid])
